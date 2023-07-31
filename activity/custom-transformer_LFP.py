fd605e573bbc25bb6d80a64aad1638a7743195ed
luisfp2000 <luisfp2000@gmail.com>
2023-07-30 21:49:10 -0600

    archivo practica modificado

--- activity/custom-transformer_LFP.py
+++ activity/custom-transformer_LFP.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 
 # TODO: Modify this list to include the numerical columns
-NUMERICAL_VARS = []
+NUMERICAL_VARS = ['pclass','survived','age','sibsp','parch','fare']
 
 # Crear custom transformer
 
@@ -15,17 +15,22 @@ class MissingIndicator(BaseEstimator, TransformerMixin):
         else:
             self.variables = variables
 
-    def fit(self, X, y):
+    def fit(self, X, y=None):
         return self
 
     def transform(self, X):
-        # TODO: Put your code here
+        # Crear una copia del DataFrame X para evitar modificar los datos originales
+        X_transformed = X.copy()
 
-        return X
+        # Para cada variable en la lista de variables
+        for variable in self.variables:
+            # Crear una nueva columna con el nombre "variable_missing" que será 1 si hay valores faltantes y 0 si no
+            X_transformed[variable + '_nan'] = X_transformed[variable].isnull().astype(int)
 
+        return X_transformed
 
 # Leer el csv sin aplicar transformaciones
-df = pd.read_csv("module-2/session-6/activity/raw-data.csv")
+df = pd.read_csv("C:/Users/luis.fernandez.COPPEL/LFPGit/session-6/activity/raw-data.csv")
 
 # Imprimir los primeros datos
 print(df.head(10))
